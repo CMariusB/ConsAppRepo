3f3b785bcad480fcbf71e9ffb7e9de83662c9822
CMariusB <mariusb.costea@yahoo>
2021-10-15 18:46:49 +0300

    dd

--- consapp/Program.cs
+++ consapp/Program.cs
@@ -7,7 +7,7 @@ namespace consapp
         static void Main(string[] args)
         {
             var tt = 99;
-            string b= 34;
+            string b= "34";
            
             Console.WriteLine("Hello World!"); 
         }
