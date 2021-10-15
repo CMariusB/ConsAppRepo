05c94a020738ff4ea885a1bd4c4d130b8acf1666
CMariusB <mariusb.costea@yahoo>
2021-10-15 18:55:34 +0300

    21

--- consapp/Program.cs
+++ consapp/Program.cs
@@ -9,7 +9,7 @@ namespace consapp
             var tt = 99;
             if (tt == 100)
             {
-                var d = 1212;
+                var d = 21;
             }
            
             Console.WriteLine("Hello World!"); 
